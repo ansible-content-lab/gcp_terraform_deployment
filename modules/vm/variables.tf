22e62a2b81650f6ffa1068b314fac9091191dc47
Sharvari Khedkar <shkhedka@redhat.com>
2024-01-23 12:16:22 -0800

    Terraform template for creating VM instances in GCP (#3)
    * Terraform template for creating VM instances in GCP
    
    * add default machine type, add var zone in root main
    
    * Add required labels to vm instance

--- modules/vm/variables.tf
+++ modules/vm/variables.tf
@@ -3,24 +3,23 @@ variable "deployment_id" {
   type = string
   validation {
     condition = (length(var.deployment_id) == 8 || length(var.deployment_id) == 0) && (can(regex("^[a-z]", var.deployment_id)) || var.deployment_id == "")
-    error_message = "deployment_id length should be 8 chars and should contain lower case alphabets only"
+    error_message = "deployment_id length should be 8 chars and should contain lower case alpha chars only"
   }
 }
 
-variable "region" {
-  description = "GCP region"
+variable "app_tag" {
+  description = "AAP tag used in VM name."
   type = string
-  default = "us-east1"
-}
-
-variable "zone" {
-  description = "GCP availability zone"
-  type = string
-  default = "us-east1-b"
 }
 
 variable "machine_type" {
   description = "Machine type to use for VM creation."
   type = string
   default = "n2-standard-2"
+}
+
+variable "zone" {
+  description = "GCP availability zone"
+  type = string
+  default = "us-east1-b"
 }
